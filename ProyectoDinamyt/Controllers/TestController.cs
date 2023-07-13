025044de0fe63ecbef301a0979ecb611df8bcc3d
reyHd00 <al060872@uacam.mx>
2023-07-13 00:38:10 -0600

    cree la clase controller tester para hacer las pruebas

---
+++ ProyectoDinamyt/Controllers/TestController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoDinamyt.Models;
+using ProyectoDinamyt.Repository.Imp;
+
+namespace ProyectoDinamyt.Controllers
+
+{
+    [ApiController]
+    [Route("[controller]")]
+    
+    public class TestController : ControllerBase
+    {
+        private IMostrarDatosRepository _mostrarDatosRepository;
+        public TestController(IMostrarDatosRepository mostrarDatosRepository)
+        {
+            _mostrarDatosRepository = mostrarDatosRepository;
+        }
+
+        [HttpGet("PrimeraPrueba")]
+        public ActionResult GetPrimeraPrueba()
+        {
+            return Ok("Hola Mundo");
+        }
+
+        [HttpGet("{nombre}")]
+        public ActionResult MostrarDatos([FromRoute] string nombre)
+        {
+            CantidadModels cantidad = _mostrarDatosRepository.GetCantidadModels(nombre);
+            return Ok(cantidad);
+        }
+
+        
+    }
+}
