34e91347324d888a30c31d022abd0b912a009e79
reyHd00 <al060872@uacam.mx>
2023-07-13 16:09:18 -0600

    agregue la clase de validacion de usuario

---
+++ ProyectoDinamyt/Repository/UserValidationRespository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoDinamyt.Repository.Acess;
+using ProyectoDinamyt.Repository.Tables;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using ProyectoDinamyt.Repository.Imp;
+
+
+namespace ProyectoDinamyt.Repository
+{
+    public class UserValidationRespository : IUserValidationRepository
+    {
+        private readonly BuscadorDbContext _context;
+        public UserValidationRespository(BuscadorDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<UsersTable> Get(string email, string password)
+        {
+            // Realiza la lógica para obtener el usuario de acuerdo con el correo electrónico y la contraseña proporcionados
+            UsersTable user = await _context.Users.FirstOrDefaultAsync(x => x.Email == email && x.Pass == password);
+            return user;
+        }
+        public async Task<UsersTable> Get(long id)
+        {
+            try
+            {
+                UsersTable user = await _context.Users.FirstOrDefaultAsync(x => x.IdUser == id);
+                return user;
+            }
+            catch (Exception ex)
+            {
+                // Manejo de errores si es necesario
+                // Puedes registrar el error o lanzar una excepción personalizada
+                throw new Exception("Error al obtener el usuario", ex);
+            }
+        }
+
+        public Task<bool> ValidateUserAsync(string Email, string Pass)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
