3320830415c8b36da255abe9a04111f5694e9bef
reyHd00 <al060872@uacam.mx>
2023-07-13 00:38:48 -0600

    cree la carpeta Models

---
+++ ProyectoDinamyt/Models/CantidadModels.cs
@@ -0,0 +1,10 @@
+﻿namespace ProyectoDinamyt.Models
+{
+    public class CantidadModels
+    {
+        public int IdUser { get; set; }
+        public string Username { get; set;}
+        public string Email { get; set;}
+        public string Pass { get; set; }
+    }
+}
